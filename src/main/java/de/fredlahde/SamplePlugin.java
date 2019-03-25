3d03e160ecd31e8ee69ddfd170c1d2522c3610f5
fred <fredlahde@gmail.com>
2019-03-25 14:42:02 +0100

    Add test for kms storage, use new kmssdk

--- src/main/java/de/fredlahde/SamplePlugin.java
+++ src/main/java/de/fredlahde/SamplePlugin.java
@@ -3,7 +3,7 @@ package de.fredlahde;
 import software.kloud.KMSPluginSDK.IKMSPlugin;
 import software.kloud.KMSPluginSDK.KMSPlugin;
 
-@KMSPlugin(author = "Fred Lahde", version = "0.1", priority = 1)
+@KMSPlugin(name = "Sample Plugin" ,author = "Fred Lahde", version = "0.1", priority = 1)
 public class SamplePlugin implements IKMSPlugin {
     @Override
     public void init() {
--- src/main/java/de/fredlahde/TestController.java
+++ src/main/java/de/fredlahde/TestController.java
@@ -3,11 +3,19 @@ package de.fredlahde;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import software.kloud.KMSPluginSDK.KMSStorage;
 
 @Controller
 public class TestController {
+
+    private final KMSStorage storage;
+
+    public TestController(KMSStorage storage) {
+        this.storage = storage;
+    }
+
     @GetMapping("/foo")
     public ResponseEntity<String> test() {
-        return ResponseEntity.ok("hello world");
+        return ResponseEntity.ok(storage.getRoot().getAbsolutePath());
     }
 }
